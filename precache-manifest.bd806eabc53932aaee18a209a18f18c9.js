0e11985405bb3ce4e747a4528962bf15f608f32f
lutungp <lntngp19@gmail.com>
2021-01-20 22:21:31 +0700

    Updates

---
+++ precache-manifest.bd806eabc53932aaee18a209a18f18c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35bbc10302d6c399932d4919e3ebeffb",
+    "url": "/my-portofolio/index.html"
+  },
+  {
+    "revision": "61d4f50494e4020ed17e",
+    "url": "/my-portofolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/my-portofolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/my-portofolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61d4f50494e4020ed17e",
+    "url": "/my-portofolio/static/js/main.1cfa5bdc.chunk.js"
+  },
+  {
+    "revision": "4b33a36912519ecb80f8",
+    "url": "/my-portofolio/static/js/runtime-main.313fec61.js"
+  }
+]);
